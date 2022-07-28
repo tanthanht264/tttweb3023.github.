f966015d7f63cff26996b09744f8a410969a13a6
tanthanht264 <94330886+tanthanht264@users.noreply.github.com>
2022-07-28 09:40:17 +0700

    Saving to Github

--- JS/scrip.js
+++ JS/scrip.js
@@ -14,7 +14,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 4000,
+        delay: 3000,
         disableOnInteraction: false,
     },
     pagination: {
